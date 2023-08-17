53ac1506776ec79c4a0281f666d0c035989cbfdf
Krishna Yadav <krishbeck@yahoo.com>
2023-08-17 15:58:34 -0500

    Updated getChoiceParameter function

--- vars/getChoiceParameters.groovy
+++ vars/getChoiceParameters.groovy
@@ -1,6 +1,4 @@
 def call() {
-    return [
-        choice(name: 'PARAMETER_NAME', choices: ['','DEV1', 'DEV2', 'DEV3'], description: 'Choose an option', choiceType: 'PT_SINGLE_SELECT'),
-        // Add more choice parameters as needed
-    ]
+ List choices =  ['','DEV1', 'DEV2', 'DEV3']
+ return choices
 }
